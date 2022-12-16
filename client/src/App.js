90b3c8b48d73f38fa7fc7a77a65efb138eea7b2d
lhp03 <60330965+lhp03@users.noreply.github.com>
2022-12-16 14:54:00 +0900

    add ga

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import SearchPage from "./pages/SearchPage";
+import ReactGA from "react-ga";
+
+ReactGA.event({
+  category: "User",
+  action: "Created an Account",
+});
+ReactGA.exception({
+  description: "An error ocurred",
+  fatal: true,
+});
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-B1HK8Z9PQQ");
+    history.listen((location) => {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
